4717f15fc16600f50634399f3d4a97a33545a900
El-checha1808 <100741370+El-checha1808@users.noreply.github.com>
2022-03-01 20:20:44 -0600

    Add files via upload

---
+++ Lectura.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void crearArchivo();
+void palabra();
+
+int main(){
+	
+	crearArchivo();
+	palabra();
+	
+	return 0;
+}
+
+void palabra(){
+	
+	ifstream file;
+	ofstream file2;
+	string texto;
+	string respuesta;
+	
+	file.open("entrada.txt", ios::in);
+	
+	if (file.fail()){
+		
+		cout << "El archivo no se pudo abrir" << endl;
+		
+		exit(1);
+	}
+	
+	while (!file.eof()){
+		
+		getline(file, texto);
+		
+		if (texto == string(texto.rbegin(), texto.rend())){
+			
+			file2.open("salida.txt", ios::out);
+			
+			respuesta = "El texto es palindromo";
+			cout << respuesta << endl;
+			
+			file2 << respuesta << endl;
+			
+		} else {
+			
+			cout << "El texto " << texto << " no es palindroma " << endl;
+		}
+		
+		break;
+	}
+	
+	file.close();
+	file2.close();
+	
+}
+
+void crearArchivo(){
+	
+	ofstream file;
+	file.open("entrada.txt", ios::out);
+	string texto;
+	
+	cout << "Ingrese texto: " << endl;
+	cin >> texto;
+	
+	file << texto << endl;
+	file.close();
+}
